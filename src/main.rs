52d2c24f50c4885748470ef3f73e3eb51783c501
eloi-menaud <eloimenaud@proton.me>
2026-05-28 21:34:07 +0200

    feat: add egui demo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+#![expect(rustdoc::missing_crate_level_docs)] // it's an example
+
+use eframe::egui;
+
+fn main() -> eframe::Result {
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+                    .with_title("Ma Boite de Dialogue")
+                    .with_app_id("mon-dialog-tool") 
+                    .with_decorations(false)
+                    .with_inner_size([400.0, 200.0])
+                    .with_resizable(false)
+                    .with_always_on_top(),
+                ..Default::default()
+    };
+    eframe::run_native(
+        "Confirm exit",
+        options,
+        Box::new(|_cc| Ok(Box::<MyApp>::default())),
+    )
+}
+
+#[derive(Default)]
+struct MyApp{}
+
+impl eframe::App for MyApp {
+    fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ui, |ui| {
+            ui.heading("Try to close the window");
+        });
+    }
 }
